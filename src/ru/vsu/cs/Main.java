e4cb7fe0533098ed56578033237d34e91583de32
itsremor <akamorganz@gmail.com>
2020-10-09 21:55:09 +0300

    Olya-name out was added

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -3,6 +3,7 @@ package ru.vsu.cs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String name = "Оля";
+        System.out.println("Привет, меня зовут " + name);
     }
 }
